06e6e419b1eb6bdb28d5b4cf741c3715edee2f1c
Brian Henning <henninb@msn.com>
2024-06-15 13:50:33 -0500

    wip

--- lead-function/index.js
+++ lead-function/index.js
@@ -25,7 +25,10 @@ const handler = async (event) => {
   const response = {
     statusCode: 200,
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
     },
     body: JSON.stringify({
       message: 'Lead generated successfully',
