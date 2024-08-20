7db5cef850ebf609d234a47d232a7d96783ebe9f
Brian Henning <henninb@gmail.com>
2024-08-20 16:53:54 -0500

    wip

--- authorizer-function/config-example.js
+++ authorizer-function/config-example.js
@@ -3,6 +3,7 @@ module.exports = {
   px_cookie_secret: "",
   px_auth_token: "",
   px_first_party_enabled: false,
+  px_logger_severity: 'debug'
   px_module_mode: 'active_blocking',
   px_bypass_monitor_header: 'x-px-block',
   px_js_ref: "https://henninb.github.io/human-challenge/human-challenge.js",
